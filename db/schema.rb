9866404b469bca5c6e0dbb7cd120cba0e36cdfd4
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-29 22:45:09 +0900

    migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_080815) do
+ActiveRecord::Schema.define(version: 2021_03_29_134123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_28_080815) do
     t.float "lat"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name_romaji"
   end
 
 end
