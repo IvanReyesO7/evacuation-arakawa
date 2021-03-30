564d579ea59ea75a28a1748e0ef1c0175cecfd73
Ivan Reyes <ivanwilf@hotmail.com>
2021-03-31 00:44:21 +0900

    seedds loading all info from json

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,18 @@ require 'open-uri'
 url = 'https://raw.githubusercontent.com/IvanReyesO7/evacuation-arakawa/master/db/database.json'
 locations = JSON.parse(open(url).read)
 
+puts "Deleting all existing locations..."
+Location.destroy_all
 locations.each do |location|
   new_location = Location.new(
     name: location["name"],
     name_romaji: location["name_romaji"],
+    name_kana: location["name_kana"],
     alt_name: location["alt_name"],
     lon: location["lon"],
     lat: location["lat"]
     )
   new_location.save
-  p "#{new_location.name} created"
+  puts "#{new_location.name} created"
 end
 
