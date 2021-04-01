7b4f377bab98a97ace9153acbe86dcfac731d022
Ivan Reyes <ivanwilf@hotmail.com>
2021-04-01 11:58:39 +0900

    home controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @locations = Location.all
+    @markers = @locations.geocoded.map do |location|
+      {
+        lat: location.lat,
+        lng: location.lon
+      }
+    end
+    raise
   end
 end
