54645800a4c165f4e23009c34400ddbbad1f7917
Ivan Reyes <ivanwilf@hotmail.com>
2021-04-02 14:10:36 +0900

    Displaying two points

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,6 +14,26 @@ const initMapbox = () => {
   if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 
+
+
+
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
     const routes = () => {
         // make a directions request using cycling profile
         // an arbitrary start will always be the same
@@ -37,61 +57,12 @@ const initMapbox = () => {
               coordinates: route
             }
           };
-          // if the route already exists on the map, reset it using setData
-          if (map.getSource('route')) {
-            map.getSource('route').setData(geojson);
-          } else { // otherwise, make a new request
-            map.addLayer({
-              id: 'route',
-              type: 'line',
-              source: {
-                type: 'geojson',
-                data: {
-                  type: 'Feature',
-                  properties: {},
-                  geometry: {
-                    type: 'LineString',
-                    coordinates: geojson
-                  }
-                }
-              },
-              layout: {
-                'line-join': 'round',
-                'line-cap': 'round'
-              },
-              paint: {
-                'line-color': '#3887be',
-                'line-width': 5,
-                'line-opacity': 0.75
-              }
-            });
-          }
-          // add turn instructions here at the end
+          console.log(geojson);
         };
-        req.send();
-      }
-
-
-
-
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      const element = document.createElement('div');
-      element.className = 'marker';
-      element.style.backgroundImage = `url('${marker.image_url}')`;
-      element.style.backgroundSize = 'contain';
-      element.style.width = '25px';
-      element.style.height = '25px';
-
-      new mapboxgl.Marker(element)
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-    });
-    routes():
+      req.send();
+    }
+    var canvas = map.getCanvasContainer();
+    routes();
     fitMapToMarkers(map, markers);
   }
 };
