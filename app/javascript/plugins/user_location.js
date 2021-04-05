81ce6c497844743322317c3a1d80742399702216
Ivan Reyes <ivanwilf@hotmail.com>
2021-04-05 12:11:00 +0900

    home btn linked to location

--- app/javascript/plugins/user_location.js
+++ app/javascript/plugins/user_location.js
@@ -4,7 +4,7 @@ const userLocation = (callback) => {
     navigator.geolocation.getCurrentPosition(position => {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
-      document.querySelector("#nearest-btn").href = `/?lon=${lon}&lat=${lat}`;
+      document.querySelector("#nearest-btn").href = `/location?lon=${lon}&lat=${lat}`;
       document.querySelector(".userlon").innerText = lon;
       document.querySelector(".userlat").innerText = lat;
     });
