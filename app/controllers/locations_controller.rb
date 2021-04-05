6e1d7e553443a8b2e4b7b5afff9880931db12459
Ivan Reyes <ivanwilf@hotmail.com>
2021-04-05 12:08:55 +0900

    new location page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,14 @@
 class LocationsController < ApplicationController
+  def location
+    @locations = Location.all
+    @markers = @locations.geocoded.map do |location|
+      {
+        lat: location.lat,
+        lng: location.lon,
+        image_url: helpers.asset_url('evacuation.jpeg'),
+        id: location.id
+      }
+    end
+    @nearest = Location.near([params[:lat], params[:lon]], 3)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'location', to: "locations#location"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
