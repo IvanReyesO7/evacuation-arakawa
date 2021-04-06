34cb0d632fe53cec5d4272bec01cfb9c84b6edc7
Ivan Reyes <ivanwilf@hotmail.com>
2021-04-06 14:20:22 +0900

    Info card

--- app/javascript/plugins/user_location.js
+++ app/javascript/plugins/user_location.js
@@ -4,7 +4,9 @@ const userLocation = (callback) => {
     navigator.geolocation.getCurrentPosition(position => {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
-      document.querySelector("#nearest-btn").href = `/location?lon=${lon}&lat=${lat}`;
+      if (document.querySelector("#nearest-btn")){
+        document.querySelector("#nearest-btn").href = `/location?lon=${lon}&lat=${lat}`;
+      }
       document.querySelector(".userlon").innerText = lon;
       document.querySelector(".userlat").innerText = lat;
     });
